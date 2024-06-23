42dbf60b8c5589a1c52221f31e59310b66564814
zenginb05 <zenginb2005@gmail.com>
2024-06-23 19:29:13 +0300

    İ am learning now

---
+++ İ am Learning Now/project.py
@@ -0,0 +1,28 @@
+OgrenciAd = "Ahmet"
+OgrenciSoyad = "Mehmet"
+OgrenciAdSoyad = OgrenciAd + " " + OgrenciSoyad
+OgrenciNumarasi = 200800100
+OgrenciCinsiyet = 1
+OgrenciKimlik = 12233344440
+OgrenciDogumYili = 2000
+OgrenciAdres = "Izmir Turkey"
+OgrenciYasi = 2024-OgrenciDogumYili
+
+
+print(OgrenciAd)
+print(OgrenciSoyad)
+print(OgrenciAdSoyad)
+print(OgrenciNumarasi)
+print(OgrenciCinsiyet)
+print(OgrenciKimlik)
+print(OgrenciDogumYili)
+print(OgrenciAdres)
+print(OgrenciYasi)
+
+"""
+Ürün 1 = 50 TL
+Ürün 2 = 60.5 TL
+Ürün 3 = 356.45 TL
+"""
+toplam = 50+60.5+356.45
+print(toplam)
